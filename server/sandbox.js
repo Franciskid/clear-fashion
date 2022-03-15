5621d094a51061dc25e04cfbb19dae9da300d175
Franciskid <Franciskid97@gmail.com>
2022-03-15 16:59:23 +0100

    ça marche

--- server/sandbox.js
+++ server/sandbox.js
@@ -4,45 +4,54 @@ const montlimartbrand = require('../server/sources/montlimartbrand.js');
 const adresseparisbrand = require('../server/sources/adresseparisbrand.js');
 
 const fs = require('fs');
+const { callbackify } = require('util');
 
 const brands = {
   Dedicated: ['https://www.dedicatedbrand.com/en/men/all-men', dedicatedbrand],
-  Montlimart: ['https://adresse.paris/630-toute-la-collection', montlimartbrand],
-  AdresseParis: ['https://www.montlimart.com/toute-la-collection.html', montlimartbrand]
+  AdresseParis: ['https://adresse.paris/630-toute-la-collection', adresseparisbrand],
+  Montlimart: ['https://www.montlimart.com/toute-la-collection.html', montlimartbrand]
 }
+
+let products = []
 
 async function sandbox(index) {
-  try
-  {
-    console.log(`🕵️‍♀️  browsing ${brands[index]} source, index :`, index);
-
-    let products = await brands[index][1].scrape(brands[index][0])
-    
-    console.log("Produits : ", products);
-    console.log('done');
-
-    fs.writeFileSync("products.json", JSON.stringify(products), 'utf8', function (err) {
-      if (err) {
-          return console.log(err);
-      }
+    try
+    {
+      console.log(`🕵️‍♀️  browsing ${brands[index]} source, index :`, index);
   
-      console.log("The file was saved!");
-  }); 
+      let _products = await brands[index][1].scrape(brands[index][0])
+      
+      console.log("Produits : ", _products);
+      console.log('done');
+      //products = products.concat(_products)
+      console.log(_products);
+      
+      products = products.concat(_products);
 
-    process.exit(0);
-  }
-  catch
-  {
-    console.error(e);
-    process.exit(1);
-  }
+      fs.writeFileSync("products.json", JSON.stringify(products), 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        
+        console.log("The file was saved!");
+        });
+
+    }
+    catch
+    {
+      console.error(e);
+    }
+  
 }
 
-async function launchScrape(){
-  Object.keys(brands).forEach(async function(key) {
-    console.log(key);
-    await sandbox(key);
-  });
-}
 
-launchScrape();
+Object.keys(brands)
+.forEach(function(key) {
+  sandbox(key);
+})
+
+
+  
+
+
+
